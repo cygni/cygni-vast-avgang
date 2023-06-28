a23d3d2d396274fc82154f787a1623ec5ae66b4c
fanny.maras <fanny.petterssonsallberg@astrazeneca.com>
2023-06-28 08:22:51 +0200

    add nordstan

--- models/Models.ts
+++ models/Models.ts
@@ -3,6 +3,7 @@ import { StopArea } from "./Trip";
 export const StopAreas: StopArea[] = [
   { name: "Kungsportsplatsen", id: "9021014004090000" },
   { name: "Brunsparken", id: "9021014001760000" },
+  { name: "Nordstan", id: "9021014004945000" },
   { name: "Centralstationen", id: "9021014001950000" },
   { name: "Centralstationen Tågstation", id: "9021014008000000" },
 ];
