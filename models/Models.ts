bfe4ec6cbe93d07b98a4b8a195e7e9a8af7aeebc
fanny.maras <fanny.petterssonsallberg@astrazeneca.com>
2023-07-18 15:53:45 +0200

    fixed position clock

--- models/Models.ts
+++ models/Models.ts
@@ -2,7 +2,7 @@ import { StopArea } from "./Trip";
 
 export const StopAreas: StopArea[] = [
   { name: "Brunnsparken", id: "9021014001760000", offset: "20", limit: "8" },
-  { name: "Nordstan", id: "9021014004945000", offset: "10", limit: "8" },
+  { name: "Nordstan", id: "9021014004945000", offset: "10", limit: "7" },
   {
     name: "Kungsportsplatsen",
     id: "9021014004090000",
